9fd4ef6854195b52f6acca4bd73a7ce5fb8d3611
/C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU </C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2007-07-06 14:16:46 +0000

    * essentially a revert to dual-signal status updates
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/software/waves/trunk@2234 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- src/visproperty.h
+++ src/visproperty.h
@@ -6,7 +6,6 @@
 #include <set>
 #include "streams.h"
 #include "streamvis.h"
-#include "wavestreamrenderer.h"
 
 class VisProperty: public Gtk::Window
 {
@@ -14,7 +13,6 @@ class VisProperty: public Gtk::Window
  public:
   VisProperty(std::set<streamVisPtr_t> *);
   void updateSet();
-  void updateProperty(streamVisPtr_t p, bool force); 
 
  private:
   Glib::RefPtr<Gnome::Glade::Xml> pGladeXml_;
@@ -23,23 +21,6 @@ class VisProperty: public Gtk::Window
   void colorSet(); 
   Gtk::Adjustment hScaleAdj_; 
   void  hScaleAdjValueChanged(); 
-  class ModelColumns : public Gtk::TreeModel::ColumnRecord
-  {
-  public:
-
-    ModelColumns()
-    { add(waveColor_); add(colorName_); }
-
-    Gtk::TreeModelColumn<WaveColor> waveColor_;
-    Gtk::TreeModelColumn<Glib::ustring> colorName_;
-  };
-
-  ModelColumns colorColumns_; 
-  Glib::RefPtr<Gtk::ListStore> pColorTreeModel_;
-  
-  Gtk::Adjustment ampScaleAdj_; 
-  
-  
 };
 
 #endif // VISPROPERTY_H
